4bbaa6ded7cbc5c8f9da59a1bc525ec9e500cbec
Matias Menendez <matias.95@hotmail.es>
2022-02-02 17:33:23 -0300

    agregado de plegado

--- clase9matiasmenendez.js
+++ clase9matiasmenendez.js
@@ -4,6 +4,7 @@
 /*Variables*/
 
 /*Valores en numero de los inputs */
+/*Corte guillotina */
 const tipoDeMaterial = document.querySelector("#tipoDeMaterial");
 const medida1H = document.querySelector("#medida1H");
 const medida2H = document.querySelector("#medida2H");
@@ -14,21 +15,34 @@ const espesorI = document.querySelector("#espesorI");
 const medida1B = document.querySelector("#medida1B");
 const medida2B = document.querySelector("#medida2B");
 const espesorB = document.querySelector("#espesorB");
+
+/*Plegado en U */
+const medida1PH = document.querySelector("#medida1PH");
+const medida2PH = document.querySelector("#medida2PH");
+const medida3PH = document.querySelector("#medida3PH");
+const espesorPH = document.querySelector("#espesorPH");
+const medida1PI = document.querySelector("#medida1PI");
+const medida2PI = document.querySelector("#medida2PI");
+const medida3PI = document.querySelector("#medida3PI");
+const espesorPI = document.querySelector("#espesorPI");
+const medida1PB = document.querySelector("#medida1PB");
+const medida2PB = document.querySelector("#medida2PB");
+const medida3PB = document.querySelector("#medida3PB");
+const espesorPB = document.querySelector("#espesorPB");
+
 var valorDolar = 110
 var pesoEspecifico = 7.2
 
 /*objetos*/
 
-const inoxidable = new Material('inoxidable', 1300, true, 7.2, medida1I, medida2I, espesorI);
-const hierro = new Material('hierro', 330, true, 7.2, medida1H, medida2H, espesorH);
-const bronce = new Material('bronce', 500, false, 7,2, medida1B, medida2B, espesorB);
-
-
-
+const inoxidable = new Material('inoxidable', 1300, 1600, true, 7.2, medida1I, medida2I, espesorI);
+const hierro = new Material('hierro', 330, 630, true, 7.2, medida1H, medida2H, espesorH);
+const bronce = new Material('bronce', 500, 800, false, 7,2, medida1B, medida2B, espesorB);
 
 
 /*Funciones*/
 
+/*Funciones corte guillotina*/
 function calcularPresupuestoPesosH(){
 let precioHierro = (medida1H.value/1000) * (medida2H.value/1000) * espesorH.value * pesoEspecifico * hierro.precioPorKilo;
 document.querySelector("#precioPesosH").value = precioHierro + "pesos"}
@@ -56,8 +70,36 @@ let precioBronceD = (medida1B.value/1000) * (medida2B.value/1000) * espesorB.val
 document.querySelector("#precioDolaresB").value = precioBronceD + "dolares"}
 
 
+/*Funciones plegados en U*/
+function calcularPresupuestoPesosPH(){
+    let precioHierroP = ((medida1PH.value*2)/1000) * (medida2PH.value/1000) * (medida3PH.value/1000) * espesorPH.value * pesoEspecifico * hierro.precioPorKiloP;
+    document.querySelector("#precioPesosPH").value = precioHierroP + "pesos"}
+    
+    function calcularPresupuestoDolarPH(){
+    let precioHierroPD = ((medida1PH.value*2)/1000) * (medida2PH.value/1000) * (medida3PH.value/1000) * espesorPH.value * pesoEspecifico * hierro.precioPorKiloP / valorDolar;
+    document.querySelector("#precioDolaresPH").value = precioHierroPD + "dolares"}
+    
+    
+    function calcularPresupuestoPesosPI(){
+    let precioInoxP = ((medida1PI.value*2)/1000) * (medida2PI.value/1000) * (medida3PI.value/1000) * espesorPI.value * pesoEspecifico * inoxidable.precioPorKiloP;
+    document.querySelector("#precioPesosPI").value = precioInoxP + "pesos"}
+      
+    function calcularPresupuestoDolarPI(){
+    let precioInoxPD = ((medida1PI.value*2)/1000) * (medida2PI.value/1000) * (medida3PI.value/1000) * espesorPI.value * pesoEspecifico * inoxidable.precioPorKiloP / valorDolar;
+    document.querySelector("#precioDolaresPI").value = precioInoxPD + "dolares"}
+    
+    
+    function calcularPresupuestoPesosPB(){
+    let precioBronceP = ((medida1PB.value*2)/1000) * (medida2PB.value/1000) * (medida3PB.value/1000) * espesorPB.value * pesoEspecifico * bronce.precioPorKiloP;
+    document.querySelector("#precioPesosPB").value = precioBronceP + "pesos"}
+      
+    function calcularPresupuestoDolarPB(){
+    let precioBroncePD = ((medida1PB.value*2)/1000) * (medida2PB.value/1000) * (medida3PB.value/1000) * espesorPB.value * pesoEspecifico * bronce.precioPorKiloP / valorDolar;
+    document.querySelector("#precioDolaresPB").value = precioBroncePD + "dolares"}
 
-/*Botones que no actualiza la pag y me brinda resultado */
+
+
+/*Botones que no actualiza la pag y me brinda resultado de corte guillotina */
 const botonPresupuestarH = document.querySelector("#botonPresupuestarH")
 
 botonPresupuestarH.addEventListener("click", (evt) => {
@@ -95,6 +137,60 @@ botonPresupuestarB.addEventListener("click", (evt) => {
  localStorage.setItem("primer medida de bronce", medida1B.value)
  localStorage.setItem("segunda medida de bronce", medida2B.value)
  localStorage.setItem("espesor de bronce", espesorB.value)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Botones que no actualiza la pag y me brinda resultado de plegado */
+const botonPresupuestarPH = document.querySelector("#botonPresupuestarPH")
+
+botonPresupuestarPH.addEventListener("click", (evt) => {
+ evt.preventDefault();
+ calcularPresupuestoPesosPH();
+ calcularPresupuestoDolarPH();
+ verificarStockHierro();
+ /*Guardar datos de medidas en local store*/
+ localStorage.setItem("primer medida de plegado hierro", medida1PH.value)
+ localStorage.setItem("segunda medida de plegado hierro", medida2PH.value)
+ localStorage.setItem("espesor de plegado hierro", espesorPH.value)
+})
+
+const botonPresupuestarPI = document.querySelector("#botonPresupuestarPI")
+
+botonPresupuestarPI.addEventListener("click", (evt) => {
+ evt.preventDefault();
+ calcularPresupuestoPesosPI();
+ calcularPresupuestoDolarPI();
+ verificarStockInoxidable();
+ /*Guardar datos de medidas en local store*/
+ localStorage.setItem("primer medida de plegado inoxidable", medida1PI.value)
+ localStorage.setItem("segunda medida de plegado inoxidable", medida2PI.value)
+ localStorage.setItem("espesor de plegado inoxidable", espesorPI.value)
+})
+
+const botonPresupuestarPB = document.querySelector("#botonPresupuestarPB")
+
+botonPresupuestarPB.addEventListener("click", (evt) => {
+ evt.preventDefault();
+ calcularPresupuestoPesosPB();
+ calcularPresupuestoDolarPB();
+ verificarStockBronce();
+ /*Guardar datos de medidas en local store*/
+ localStorage.setItem("primer medida de plegado bronce", medida1PB.value)
+ localStorage.setItem("segunda medida de plegado bronce", medida2PB.value)
+ localStorage.setItem("espesor de plegado bronce", espesorPB.value)
 })
 
 
--- object.js
+++ object.js
@@ -1,8 +1,9 @@
 /* OBJETOS*/ 
 class Material {
-    constructor(nombre, precioPorKilo, disponibilidad, pesoEspecifico, medida1, medida2, espesor) {
+    constructor(nombre, precioPorKilo, precioPorKiloP, disponibilidad, pesoEspecifico, medida1, medida2, espesor) {
         this.nombre = nombre;
         this.precioPorKilo = precioPorKilo;
+        this.precioPorKiloP = precioPorKiloP;
         this.disponibilidad = disponibilidad;
         this.pesoEspecifico = pesoEspecifico;
         this.medida1 = medida1;
